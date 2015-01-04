aaaff02218f4d24bcc365005f267f1a13be53c2c
Fahad Shami <Fahad Shami@192.168.1.3>
2015-01-04 14:25:54 +0500

    Chap 19: Set The Table (Used @Before)

--- src/com/tddpart2/TestCaseTest.java
+++ src/com/tddpart2/TestCaseTest.java
@@ -2,15 +2,20 @@ package com.tddpart2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCaseTest extends TestCase 
 {
 	WasRun test;
+	@Before
+	public void setUp()
+	{
+		this.test= new WasRun("testMethod");
+	}
 	@Test
 	public void testWasRun()
 	{
-		WasRun test = new WasRun("testMethod");
 		System.out.println(test.wasRun);
 		assertEquals(false,test.wasRun);
 		test.testMethod();
@@ -21,7 +26,6 @@ public class TestCaseTest extends TestCase
 	@Test
 	public void testForMethodRun()
 	{
-		WasRun test = new WasRun("testMethod");
 		System.out.println(test.wasRun);
 		assertEquals(false,test.wasRun);
 		test.run();
@@ -29,21 +33,15 @@ public class TestCaseTest extends TestCase
 		assertEquals(true,test.wasRun);
 
 	}
-	public void setUp()
-	{
-		this.test= new WasRun("testMethod");
-	}
 	@Test
 	public void testRunning() 
 	{
-		setUp();//should be called polymorphically
 		this.test.run();
 		System.out.println(this.test.wasRun);
 		assertEquals(true,test.wasRun);
 	}
 	@Test
 	public void testSetUp(){
-		setUp();
 		this.test.run();
 		assertEquals(true,test.wasSetUp);
 	}
